10ade98e40e06c5860ec366546142a73050c36a8
Delmy <dfajardob@miumg.gt>
2024-10-13 22:32:19 -0600

    calculadora

---
+++ app/src/main/java/com/delmy/caculadora/components/CardComponents.kt
@@ -0,0 +1,50 @@
+package com.delmy.caculadora.components
+
+import android.icu.text.IDNA.Info
+import android.widget.Space
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun InfoCard(title:String, info:Double, modifier: Modifier){
+    Card(
+        modifier = modifier,
+        colors=CardDefaults.cardColors(
+            containerColor = Color.LightGray
+        )
+    ){
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp)
+        ){
+            Text(text = title, color = Color.Black, fontSize = 20.sp)
+            Text(text = "GTQ. $info", color = Color.Black, fontSize = 20.sp)
+
+        }
+    }
+
+}
+@Composable
+fun ShowInfoCards(titleIntereses:String, montoInteres:Double, titleMonto:String, monto:Double){
+    Row(){
+        InfoCard(title = titleIntereses, info= montoInteres, modifier = Modifier.padding(30.dp).weight(1f))
+        SpaceV (10.dp)
+        InfoCard(title = titleMonto, info= monto, modifier = Modifier.padding(30.dp).weight(1f))
+
+
+
+    }
+}
---
+++ app/src/main/java/com/delmy/caculadora/views/HomeView.kt
@@ -0,0 +1,120 @@
+package com.delmy.caculadora.views
+
+import android.icu.math.BigDecimal
+import android.renderscript.Sampler.Value
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.delmy.caculadora.components.Alert
+import com.delmy.caculadora.components.MainButton
+import com.delmy.caculadora.components.MainTextField
+import com.delmy.caculadora.components.ShowInfoCards
+import com.delmy.caculadora.components.SpaceH
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeView(){
+    Scaffold (topBar = {
+        CenterAlignedTopAppBar(title = {Text(text= "Calculadora de Cuotas")},
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.surface
+            )
+        )
+    }){
+        ContentHomeView(it)
+    }
+}
+
+
+@Composable
+fun ContentHomeView(paddingValues: PaddingValues){
+    var montoPrestamo by remember { mutableStateOf (value =  "")}
+    var cantCuotas by remember {mutableStateOf (value = "") }
+    var tasa by remember {mutableStateOf (value =  "")}
+    var montoInteres by remember {mutableStateOf (value =   0.0)}
+    var montoCuota by remember {mutableStateOf (value =  0.0)}
+    var showAlert by remember {mutableStateOf (value = false)}
+
+    Column( modifier= Modifier
+        .padding(paddingValues)
+        .padding(10.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally){
+        ShowInfoCards(
+            titleIntereses = "Total",
+            montoInteres =  montoInteres,
+            titleMonto = "Monto",
+            monto = montoCuota
+        )
+
+        MainTextField(value = montoPrestamo, onValueChange={montoPrestamo = it }, label = "Prestamo")
+        SpaceH()
+        MainTextField(value =cantCuotas , onValueChange={cantCuotas = it }, label = "cuotas")
+        SpaceH(10.dp)
+        MainTextField(value = tasa, onValueChange = {tasa = it}, label = "Tasa")
+        SpaceH(20.dp)
+        MainButton (text = "Calcular"){
+            if (montoPrestamo != "" && cantCuotas !=""){
+                montoInteres= calcularTotal (montoPrestamo.toDouble(), cantCuotas.toInt(), tasa.toDouble())
+                montoCuota = calcularCuota(montoPrestamo.toDouble(), cantCuotas.toInt(), tasa.toDouble())
+            }else {
+                showAlert= true
+            }
+        }
+        SpaceH()
+        MainButton(text = "Borrar", color = Color.Red) {
+            montoPrestamo=""
+            cantCuotas=""
+            tasa=""
+            montoInteres=0.0
+            montoCuota= 0.0
+        }
+        if (showAlert){
+            Alert(
+                 title = "Alerta" ,
+                 message = "por fabor ingrese los datos ",
+                confirmText="Aceptar",
+                onConfirmClick = {
+                showAlert = false }
+                ){}
+            }
+        }
+    }
+
+
+
+
+
+fun calcularTotal(monto : Double, cuotas: Int, tasa: Double): Double {
+    val res = cuotas * calcularCuota (monto, cuotas, tasa)
+    return BigDecimal(res).setScale(2,BigDecimal.ROUND_UP).toDouble()
+}
+
+fun calcularCuota(monto: Double, cuotas: Int, tasa: Double): Double {
+    val tasaInteresesMensual = tasa /12/100
+    val cuota = monto * tasaInteresesMensual* Math.pow(1*tasaInteresesMensual,cuotas.toDouble())/(Math.pow(1+tasaInteresesMensual,cuotas.toDouble())-1)
+
+    val cuotaRedondeada = BigDecimal(cuota).setScale(2,BigDecimal.ROUND_UP).toDouble()
+    return cuotaRedondeada
+
+}
+
+
